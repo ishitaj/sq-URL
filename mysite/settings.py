1836df41a85d3ef7eccd5094937144d5ed08af3b
ijain89 <ijain89@gmail.com>
2017-08-14 11:41:05 +0530

    Pushing squashed migrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'ishita',
-        'PASSWORD': 'jlj',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
     }
 }
